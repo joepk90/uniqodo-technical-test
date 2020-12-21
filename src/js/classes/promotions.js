8e0f7de9f670f9f1a1dd717a253e8e18d6c9ce2a
Joe <jparkkennaby@gmail.com>
2020-12-21 16:54:59 +0000

    update Promotions class to return array of promotional codes

--- src/js/classes/promotions.js
+++ src/js/classes/promotions.js
@@ -1,12 +1,35 @@
 const Promotions = class {
 
     constructor() {
+        this.promotionalDomElements = this.getPromotionDomElements();
+    }
+
+    getPromotionDomElements() {
+        return document.getElementsByClassName('promo');
+    }
+
+    /**
+     * check for valid promtional element
+     */
+    getPromotionElement(element) {
+
+        if (element.childElementCount === 0) {
+            return element.innerText;
+        }
+
+        return element.lastChild.innerText;
+
     }
 
     getPromotionsElements() {
 
-        return document.getElementsByClassName('promo');
+        if (!this.promotionalDomElements || this.promotionalDomElements.length === 0) {
+            return [];
+        }
 
+        return Array.from(this.promotionalDomElements).map(promotionalDomElement => {
+            return this.getPromotionElement(promotionalDomElement)
+        });
     }
 
 }
